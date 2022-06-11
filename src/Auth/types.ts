2e8b838b33837b383e519fa55e541c79d17d7681
mzagumennyy <mzagumennyy@digitexfutures.com>
2022-06-11 11:31:15 +0200

    updated auth types: added AuthenticatedState

--- src/Auth/types.ts
+++ src/Auth/types.ts
@@ -5,9 +5,18 @@ export type User = {
     readonly lastName: string;
 };
 
+export type AuthenticatedState =
+    | {
+          readonly isAuthenticated: false;
+      }
+    | {
+          readonly isAuthenticated: true;
+          readonly user: User;
+      };
+
 export type AuthService = {
     readonly register: (email: string, password: string) => Promise<User>;
     readonly login: (email: string, password: string) => Promise<User>;
     readonly logout: () => void;
-    readonly getCurrentUser: () => User | null;
+    readonly getCurrentUser: () => AuthenticatedState;
 };
